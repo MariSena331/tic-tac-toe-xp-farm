5ab5dfbaaf01695f51319fb2b95bc6289bc3a330
MariSena331 <mariana7007@hotmail.com>
2023-02-25 09:41:13 -0300

    chore: handle game turn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { React, useState } from 'react';
+import { React, useState, useRef } from 'react';
 
 function App() {
   const [player1, setPlayer1] = useState({
@@ -13,21 +13,17 @@ function App() {
   const [turn, setTurn] = useState(0);
   const [endGame, setEndGame] = useState(false);
   const [currentPlayer, setCurrentPlayer] = useState('');
-  const [selectedLayer, setSelectedLayer] = useState();
 
-  const handleGameTurn = (layer) => {
-    if (turn === 0 || turn === 2 || turn === 4 || turn === 6 || turn === 8) {
-      setCurrentPlayer(player1.symbol);
-    }
-    if (turn === 1 || turn === 3 || turn === 5 || turn === 7) {
-      setCurrentPlayer(player2.symbol);
-    }
-    setSelectedLayer(layer);
-    setTurn(prevState => prevState + 1);
+  const handleGameTurn = () => {
     if(turn === 8) {
-      setEndGame(true);
+      return setEndGame(true);
     }
-    return;
+
+    return setTurn(prevState => prevState + 1);
+  }
+
+  const handlePlayerClick = layer => {
+    handleGameTurn();
   }
 
   return (
@@ -38,45 +34,35 @@ function App() {
 
       <div className="game-panel">
         <div className="players-panel">
-          <p className={currentPlayer === player1.symbol ? 'current-player' : ''}>Player 1 {player1.symbol}</p>
-          <p className={currentPlayer === player2.symbol ? 'current-player' : ''}>Player 2 {player2.symbol}</p>
+          <p
+            className={currentPlayer === player1.symbol ? 'current-player' : ''}
+          >
+              Player 1 {player1.symbol}
+          </p>
+          <p
+            className={currentPlayer === player2.symbol ? 'current-player' : ''}
+          >
+            Player 2 {player2.symbol}
+          </p>
         </div>
         <div className="grid-area">
           {
             !endGame ? (
               <>
                 <div>
-                  <div id="0" className="grid" onClick={() => handleGameTurn(0)}>
-                    {currentPlayer && selectedLayer === 0 && currentPlayer}
-                  </div>
-                  <div className="grid" onClick={() => handleGameTurn(1)}>
-                    {currentPlayer && selectedLayer === 1 && currentPlayer}
-                  </div>
-                  <div className="grid" onClick={() => handleGameTurn(2)}>
-                    {currentPlayer && selectedLayer === 2 && currentPlayer}
-                  </div>
+                  <div className="grid" onClick={() => handlePlayerClick(0)} />
+                  <div className="grid" onClick={() => handlePlayerClick(1)} />
+                  <div className="grid" onClick={() => handlePlayerClick(2)} />
                 </div>
                 <div>
-                  <div className="grid" onClick={() => handleGameTurn(3)}>
-                    {currentPlayer && selectedLayer === 3 && currentPlayer}
-                  </div>
-                  <div className="grid" onClick={() => handleGameTurn(4)}>
-                    {currentPlayer && selectedLayer === 4 && currentPlayer}
-                  </div>
-                  <div className="grid" onClick={() => handleGameTurn(5)}>
-                    {currentPlayer && selectedLayer === 5 && currentPlayer}
-                  </div>
+                  <div className="grid" onClick={() => handlePlayerClick(3)} />
+                  <div className="grid" onClick={() => handlePlayerClick(4)} />
+                  <div className="grid" onClick={() => handlePlayerClick(5)} />
                 </div>
                 <div>
-                  <div className="grid" onClick={() => handleGameTurn(6)}>
-                    {currentPlayer && selectedLayer === 6 && currentPlayer}
-                  </div>
-                  <div className="grid" onClick={() => handleGameTurn(7)}>
-                    {currentPlayer && selectedLayer === 7 && currentPlayer}
-                  </div>
-                  <div className="grid" onClick={() => handleGameTurn(8)}>
-                    {currentPlayer && selectedLayer === 8 && currentPlayer}
-                  </div>
+                  <div className="grid" onClick={() => handlePlayerClick(6)} />
+                  <div className="grid" onClick={() => handlePlayerClick(7)} />
+                  <div className="grid" onClick={() => handlePlayerClick(8)} />
                 </div>
               </>
             ) : <p>End game</p>
